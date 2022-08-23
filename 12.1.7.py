c59cdebe645be3a8d759326881adb0eda38ce13c
Yury Matveev <matveev.ya@gmail.com>
2022-08-23 10:43:56 +0300

    Initial Commit

---
+++ 12.1.7.py
@@ -0,0 +1,8 @@
+# Вставьте правильное название функции вместо ???
+def which_month():
+    month = input("Which month is it now?")
+    print("Current month is", month)
+
+
+if __name__ == '__main__':
+    which_month()
